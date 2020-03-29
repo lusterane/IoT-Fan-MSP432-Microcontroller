904323eda32cfc29950c8538defcbf5d16c78ed6
Toby Chow <tobychow98@gmail.com>
2020-03-29 02:53:00 -0500

    Added IoT WiFi connectivity

--- Fan_MSP432/Fan_MSP432.ino
+++ Fan_MSP432/Fan_MSP432.ino
@@ -1,53 +1,171 @@
-// input variables
-int fanSpeed = 100;
-int buttonOneState = 0;
-int buttonTwoState = 0;
+#ifndef __CC3200R1M1RGC__
+// Do not include SPI for CC3200 LaunchPad
+#include <SPI.h>
+#endif
+#include <WiFi.h>
+#include <Sensitive.h> // CUSTOM LINKING PATH. REMOVE IF YOU ARE NOT ME
 
-// set pin numbers
+// your network name also called SSID
+char ssid[] = sSSID; // replace sSSID with ssid of network
+// your network password
+char password[] = sPASSWORD; // replace sPASSWORD with password of network
+// your network key Index number (needed only for WEP)
+int keyIndex = 0;
+
+// fan data
+int fanSpeed = 0;
 const int pwmPin = P3_2; // pwm pin
 const int tachPin = P6_1; // tach pin
-const int buttonOne = 73;     // the number of the pushbutton pin
-const int buttonTwo = 74;     // the number of the pushbutton pin
-const int ledYellow = 78;
-const int ledBlue =  77;      // the number of the LED pin
-const int ledRed =  75;      // the number of the LED pin
+
+WiFiServer server(80);
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(115200);      // initialize serial communication
+  pinMode(BLUE_LED, OUTPUT);
+  pinMode(RED_LED, OUTPUT);      // set the LED pin mode
+  pinMode(pwmPin, INPUT);  // set PWM pin mode
 
-  pinMode(pwmPin, INPUT);
-  pinMode(ledRed, OUTPUT);
-  pinMode(ledBlue, OUTPUT); // blue
-  pinMode(buttonOne, INPUT_PULLUP); // PUSH 1
-  pinMode(buttonTwo, INPUT_PULLUP); // PUSH 2
+  // initialize Fan Speed to 0% duty cycle
+  analogWrite(pwmPin, 0);
 
+  // attempt to connect to Wifi network:
+  Serial.print("Attempting to connect to Network named: ");
+  // print the network name (SSID);
+  Serial.println(ssid); 
+  // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+  WiFi.begin(ssid, password);
+  while ( WiFi.status() != WL_CONNECTED) {
+    // print dots while we wait to connect
+    Serial.print(".");
+    delay(300);
+  }
+  
+  Serial.println("\nYou're connected to the network");
+  Serial.println("Waiting for an ip address");
+  
+  while (WiFi.localIP() == INADDR_NONE) {
+    // print dots while we wait for an ip addresss
+    Serial.print(".");
+    delay(300);
+  }
+
+  Serial.println("\nIP Address obtained");
+  
+  // you're connected now, so print out the status  
+  printWifiStatus();
+
+
+  
+  
+  Serial.println("Starting webserver on port 80");
+  server.begin();                           // start the web server on port 80
+  Serial.println("Webserver started!");
 }
-int boop = 0;
-void loop() {
-  // put your main code here, to run repeatedly: 
-  buttonOneState = digitalRead(buttonOne);
-  buttonTwoState = digitalRead(buttonTwo);
 
-  if(buttonOneState == LOW && fanSpeed > 0){ // LOW MEANS PRESSED
-    fanSpeed -= 51;
-    digitalWrite(ledRed, HIGH);
-    delay(300);
-    digitalWrite(ledRed, LOW);
+void loop() {
+  int i = 0;
+  WiFiClient client = server.available();   // listen for incoming clients
+
+  if (client) {                             // if you get a client,
+    Serial.println("new client");           // print a message out the serial port
+    char buffer[150] = {0};                 // make a buffer to hold incoming data
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        if (c == '\n') {                    // if the byte is a newline character
+
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (strlen(buffer) == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+
+            // the content of the HTTP response follows the header:
+            client.println("<html><head><title>Energia CC3100 WiFi Web Server</title></head><body align=center>");
+            client.println("<h1 align=center><font color=\"red\">CC3100 WiFi Web Server</font></h1>");
+            client.print("FAN SPEED <button onclick=\"location.href='/DECREASE_FAN'\">DECREASE</button>");
+            client.println(" <button onclick=\"location.href='/INCREASE_FAN'\">INCREASE</button><br>");
+            client.print("PWM Duty Cycle: ");
+            client.print(fanSpeed);
+            client.print("/255 (");
+            client.print(100.0*fanSpeed/255, 4);
+            client.println("%)");
+
+            // The HTTP response ends with another blank line:
+            client.println();
+            // break out of the while loop:
+            break;
+          }
+          else {      // if you got a newline, then clear the buffer:
+            memset(buffer, 0, 150);
+            i = 0;
+          }
+        }
+        else if (c != '\r') {    // if you got anything else but a carriage return character,
+          buffer[i++] = c;      // add it to the end of the currentLine
+        }
+
+        // Check to see if the client request was "GET /INCREASE_FAN" or "GET /DECREASE_FAN":
+        if (endsWith(buffer, "GET /INCREASE_FAN") && fanSpeed < 255) {
+          fanSpeed += 35;
+          digitalWrite(BLUE_LED, HIGH);
+          delay(50);
+          digitalWrite(BLUE_LED, LOW);
+        }
+        else if (endsWith(buffer, "GET /DECREASE_FAN") && fanSpeed > 0) {
+          fanSpeed -= 35;
+          digitalWrite(RED_LED, HIGH);
+          delay(50);
+          digitalWrite(RED_LED, LOW);
+        }
+        analogWrite(pwmPin, fanSpeed); // 0-255
+      }
+    }
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
   }
-  if(buttonTwoState == LOW && fanSpeed < 255) {
-    fanSpeed += 51;
-    digitalWrite(ledBlue, HIGH);
-    delay(300);
-    digitalWrite(ledBlue, LOW);
+}
+
+//
+//a way to check if one array ends with another array
+//
+boolean endsWith(char* inString, const char* compString) {
+  int compLength = strlen(compString);
+  int strLength = strlen(inString);
+  
+  //compare the last "compLength" values of the inString
+  int i;
+  for (i = 0; i < compLength; i++) {
+    char a = inString[(strLength - 1) - i];
+    char b = compString[(compLength - 1) - i];
+    if (a != b) {
+      return false;
+    }
   }
-  
-  analogWrite(pwmPin, fanSpeed); // 0-255
-  /*
-  Serial.print("(pwmPin) PWM: ");
-  Serial.print(fanSpeed, DEC);
-  Serial.print("% | ");
-*/
-  //Serial.println((String)"Tach: " + pulseIn(tachPin, HIGH));
-  Serial.println(pulseIn(tachPin, HIGH));
-  
+  return true;
+}
+
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+  // print where to go in a browser:
+  Serial.print("To see this page in action, open a browser to http://");
+  Serial.println(ip);
 }
